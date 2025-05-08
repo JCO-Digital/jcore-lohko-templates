581d8965439fd95e0c98c2f387edf5e660f0fdcf
Niklas Schönberg <niklas@uplink.fi>
2025-05-08 16:28:58 +0300

    feat: Added templates

---
+++ static/edit.js
@@ -0,0 +1,39 @@
+import { __ } from "@wordpress/i18n";
+import { InspectorControls, useBlockProps } from "@wordpress/block-editor";
+import { PanelBody, TextControl } from "@wordpress/components";
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import "./editor.css";
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {Element} Element to render.
+ */
+export default function Edit({ attributes, setAttributes }) {
+  return (
+    <>
+      <InspectorControls>
+        <PanelBody title={__("Settings")}>
+          <TextControl
+            label="Test"
+            value={attributes.test}
+            onChange={(newValue) => setAttributes({ test: newValue })}
+          />
+        </PanelBody>
+      </InspectorControls>
+
+      <p {...useBlockProps()}>
+        {__("Example Static – hello from the editor!", "jcore")}
+      </p>
+    </>
+  );
+}
